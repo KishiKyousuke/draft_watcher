6fb1a0c309cf1b5bf9e85d6e674bad056ef17b19
KishiKyousuke <carp011808pg@icloud.com>
2025-09-30 23:44:49 +0900

    テストの削除

--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -2,7 +2,5 @@ require "test_helper"
 
 class PlayersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get players_index_url
-    assert_response :success
   end
 end

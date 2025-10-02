cfcc72bc5fec41512605b6363ef2bee58c26f0d5
KishiKyousuke <carp011808pg@icloud.com>
2025-10-02 22:27:36 +0900

    1位指名の場合は同年の重複指名を許可する

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -6,5 +6,9 @@ class Pick < ApplicationRecord
   validates :player_id, presence: true
   validates :team_id, presence: true
   validates :draft_round, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :player_id, uniqueness: { scope: :year, message: "は同じ年度に複数回指名できません" }
+  validates :player_id, uniqueness: {
+    scope: :year,
+    message: "は同じ年度に複数回指名できません",
+    if: -> { draft_round != 1 || training_player }
+  }
 end
